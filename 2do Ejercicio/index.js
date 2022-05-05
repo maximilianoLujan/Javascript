095402970e2fe33d70a5b1e0889c9acfcd68a0bc
maximiliano <maxylujan1@outlook.com>
2022-05-05 14:08:06 -0300

    Segundo ejercicio del bootcamp

---
+++ 2do Ejercicio/index.js
@@ -0,0 +1,8 @@
+var cadena="Esto es una cadena de texto";
+
+function funcion(){
+    var cadena="Esto es una cadena dentro de una funcion";
+    console.log(cadena);
+}
+funcion();
+console.log(cadena);
