39608cbf886cb681f084873528b266aa3d9162b6
maximiliano <maxylujan1@outlook.com>
2022-05-05 21:21:28 -0300

    Ejercicio con break y continue

---
+++ 4to Ejercicio/Enunciado.txt
@@ -0,0 +1,1 @@
+En este segundo ejercicio, tendréis que crear un bucle que haga 10 iteraciones y en cada una indique el número de esta. Las iteraciones 2, 3, 5 y 7 deben imprimir en su lugar "Número primo".
---
+++ 4to Ejercicio/index.js
@@ -0,0 +1,7 @@
+for (let i=1; i <= 10;i++){
+    if (i ===2 || i===3 || i===5 || i===7){
+        console.log("NUMERO PRIMO");
+        continue
+    }
+    console.log(`Esta es la iteracion numero ${i} `);
+}
