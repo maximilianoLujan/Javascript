19d9393862aa9135c9393f5e0613c569542ef120
maximiliano <maxylujan1@outlook.com>
2022-05-06 13:21:00 -0300

    Pasando cadena de caracteres a mayusculas

--- 5to Ejercicio/index.js
+++ 5to Ejercicio/index.js
@@ -1,2 +1,8 @@
+//Reemplazando vecina por vecion
 let cadena ="El perro de mi vecina es muy molesto";
 console.log(cadena.replace("vecina","vecino"));
+
+//Convirtiendo cacdena de texto a mayusculas
+
+let cadena2 ="Esto es una cadena de texto";
+console.log(cadena2.toUpperCase());
