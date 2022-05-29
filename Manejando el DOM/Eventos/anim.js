ade0b0a35ca75f5d8e5d562df4be2f5eb9ff2a47
Maximiliano <maxylujan1@outlook.com>
2022-05-29 16:32:53 -0300

    Trabajando con el flujo de los eventos

--- Manejando el DOM/Eventos/anim.js
+++ Manejando el DOM/Eventos/anim.js
@@ -11,8 +11,18 @@ $miid.onclick = (e=>{
 
 const $miid2 = document.getElementById("boton-multiple");
 $miid2.addEventListener("click",e=>{
-    alert("A ver si anda el picachu")
+    alert("Este es el boton multiple")
 })
 $miid2.addEventListener("click",e=>{
-    alert("A ver si anda el picachu multiple")
+    alert("Este es el boton multiple 2")
+})
+
+//Manejando el flujo de eventos
+const $miscontenedores = document.querySelectorAll(".miseccion div");
+console.log($miscontenedores);
+function funcionEvento(e){
+    console.log(`Este es el div numero ${this.className}`)
+}
+$miscontenedores.forEach(div =>{
+    div.addEventListener("click",funcionEvento);
 })
