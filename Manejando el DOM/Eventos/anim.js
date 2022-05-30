d859348136a1778f8fd32c9ff81090eb5857525c
Maximiliano <maxylujan1@outlook.com>
2022-05-30 14:27:40 -0300

    Creando eventos con parametros y eliminando eventos

--- Manejando el DOM/Eventos/anim.js
+++ Manejando el DOM/Eventos/anim.js
@@ -16,6 +16,36 @@ $miid2.addEventListener("click",e=>{
 $miid2.addEventListener("click",e=>{
     alert("Este es el boton multiple 2")
 })
+//Realizando un evento con parametros
+const $miparam = document.getElementById("boton-parametros");
+const lafuncion = (nombre="")=>{
+    alert(`Hola ${nombre}, como andas?`)
+}
+
+$miparam.addEventListener("dblclick", ()=>{
+    lafuncion("Karin");
+})
+//Eliminando un evento
+const $botonaeliminar = document.getElementById("boton-eliminar");
+
+function eliminandoEvento(){
+    alert(`Este evento ha sido eliminado`);
+    $botonaeliminar.removeEventListener("dblclick",eliminandoEvento);
+    $botonaeliminar.disabled = true;
+}
+
+
+$botonaeliminar.addEventListener("dblclick",eliminandoEvento);
+
+
+
+
+
+
+
+
+
+
 
 //Manejando el flujo de eventos
 const $miscontenedores = document.querySelectorAll(".miseccion div");
